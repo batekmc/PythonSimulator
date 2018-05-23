02c5115e12529c716dbeaf350269e372053e04b0
batekmc <batekmc@users.noreply.github.com>
2018-05-23 15:09:37 +0200

    Major changes, added battery

--- Simulator.py
+++ Simulator.py
@@ -7,48 +7,65 @@ import pandas as pd
 
 class Sensor(object):
     
-    sensorData = {}
-    sensorName = ""
+    _sensorData = {}
+    _sensorName = ""
+    _battery = None
+    
     
     def __init__(self, name):
-        self.sensorName = name
+        self._sensorName = name
+        self._battery = Battery(100, 1)
         print ("Sensor " + name + " created")
         
     def SetDictOfData(self, data):
-        self.sensorData = data
+        self._sensorData = data
     
     def GetOutputBasedOnTimestamp(self, timestamp):
-        return self.sensorData[timestamp]
+        hasBattery = _battery.dischargePower(timestamp)
+        return self._sensorData[timestamp] if hasBattery else "Battery discharged!"
+    
+    def batteryLeft(self):
+        return self._battery.capacityLeft()
     
 
 class ExcelFileImporter(object):
 
-    fileName = ""
+    _fileName = ""
     
     def __init__(self, fileName):
-        self.fileName = fileName
+        self._fileName = fileName
         
     '''
     Rturns DataFrame object,
-    which is firs sheet in given excel file
+    which is first sheet in given excel file
     '''
     def loadFile(self):
-        file = pd.ExcelFile(self.fileName)
+        file = pd.ExcelFile(self._fileName)
         return file.parse(file.sheet_names[0])
     
+    
+'''
 
+Class for carrying global data.
+variables intended to use as static
+'''
+class DTO(object):
+    _times = None
+    
+    
+    
 class DataInitializer(object):
     
-    excelFileName = ""
+    _excelFileName = ""
     
     def __init__(self, excelFileName):
-        self.excelFileName = excelFileName
+        self._excelFileName = excelFileName
         print ("Data initialization begins!")
         
     def LOadDataAndReturnSensors(self):
         
         sensors = []
-        excelFile = ExcelFileImporter(self.excelFileName)
+        excelFile = ExcelFileImporter(self._excelFileName)
         importer = excelFile.loadFile()
         
         '''
@@ -60,6 +77,9 @@ class DataInitializer(object):
         
         timestamps = importer[importer.columns[1]]
         timestamps = timestamps[1:]
+        
+        # store defined times for later use
+        DTO._times = timestamps
         
         for i in range(2, len(importer.columns)):
             
@@ -74,23 +94,75 @@ class DataInitializer(object):
         
         return sensors
             
+class Battery(object):
+    
+    _capacity = 0
+    _energyStep = 0
+    _lastTime = -1
+    _firstTimeCheck = True
+    
+    def __init__(self, capacity= 100, energyStep= 1):
+        _capacity = capacity
+        _energyStep = energyStep
+    
+    # returns true, if there is power left
+    def dischargePower(self, time):
+        if _firstTimeCheck:
+            _capacity -= _energyStep
+            _lastTime = time
+            _firstTimeCheck = False
+        else:
+            timeDiff = time - _lastTime
+            _capacity -= _energyStep
+            _lastTime = time
+        return _capacity > 0
+    
+    def capacityLeft(self):
+        return _capacity
             
+
+class RunSimulation(object):
+    
+    _sensorsList = []
+    _verbose = False
+    _excelDataPath = r""
+    
+    def __init__(self,excelDataPath,  verbose = False):
+        _excelDataPath = excelDataPath
+        _verbose = verbose
+        print("simulatin initalized!")
+        
+    def printV(self, text):
+        if _verbose:
+            print(text)
+        
+        
+    def runSimulation(self):
+        dataIOnitializer = DataInitializer(self._excelDataPath)
+        _sensorList = dataIOnitializer.LOadDataAndReturnSensors()
+        
+        for time in DTO._times:
             
+            for sensor in _sensorList:
+                
+                printV(sensor._sensorName + ";CO2_level:" + ";battery_left:" + sensor.batteryLeft())
+
+
+##########################################################################################################
+xslPath = r"C:\Users\batiha\Downloads\Data for UNI Ostrava\0010 CO-Sensorvalues on event base.xlsx"
+
+simulation = RunSimulation(xslPath, True)                    
+
+simulation.runSimulation()
             
+        
+        
         
         
 
 
 
-var = DataInitializer(r"C:\Users\batek\Downloads\Data for UNI Ostrava\0010 CO-Sensorvalues on event base.xlsx")
 
 
-print(var.LOadDataAndReturnSensors()[3].sensorData)
 
-    
-    
-            
-    
-    
-    
-        
+
